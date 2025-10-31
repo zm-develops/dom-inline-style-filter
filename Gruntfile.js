7965f01f2ba5152867acdd77e1e2348e48be91f1
zm-toolroom <241188374+zm-toolroom@users.noreply.github.com>
2025-10-31 18:50:11 +0000

    Add test suite & switch from Rollup to Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		jshint: {
+			files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+			options: {
+				jshintrc: true,
+			},
+		},
+		karma: {
+			unit: {
+				background: false,
+				configFile: 'karma.conf.js',
+				singleRun: true,
+			},
+		},
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			dist: {
+				files: {
+					'dist/index.min.js': ['src/index.js'],
+				},
+			},
+			options: {
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n',
+				sourceMap: true,
+			},
+		},
+		watch: {
+			files: ['<%= jshint.files %>'],
+			tasks: ['test'],
+		},
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-karma');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('test', ['karma']);
+	grunt.registerTask('build', ['jshint', 'uglify']);
+	grunt.registerTask('default', ['jshint', 'test', 'uglify']);
+};
