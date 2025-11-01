4422a22b8b393433b4a58de7793e3c21c7ef5437
zm-toolroom <241188374+zm-toolroom@users.noreply.github.com>
2025-11-01 19:46:34 +0000

    Use Jasmine expect style

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,5 @@
-['code', 'wiki'].forEach(image => {
+/* eslint-disable no-undef */
+['code', 'wiki'].forEach(async image => {
 
 	const bench =  document.querySelector('div#test-bench');
 
@@ -13,29 +14,29 @@
 		.map(transformer)
 		.map(decl => Object.assign({}, decl));
 
-	const computedStylesFn = getCachedDeclaration(el => globalThis.getComputedStyle(el));
+	const computedStylesFn = getCachedDeclaration(el => getComputedStyle(el));
 	const inlineStylesFn = getCachedDeclaration(el => el.style);
 
-	const info = console.info;
+	const debug = console.debug;
 	let buffer = [];
-	const infoSpy = function() {
+	const debugSpy = function() {
 		buffer.push([...arguments].join(' '));
-		info.call(console, ...arguments);
+		debug.call(console, ...arguments);
 	};
-	console.info = infoSpy;
+	console.debug = debugSpy;
 
 	const computedStylesBefore = computedStylesFn();
 
-	globalThis.dominlinestylefilter(root, { debug: true });
+	await globalThis.dominlinestylefilter(root, { debug: true });
 
-	console.info = info;
+	console.debug = debug;
 
 	const generalData = {};
 	const authorData = { bytes: [], declarations: [] };
 	const activeData = { bytes: [], declarations: [], deltas: [] };
 	let isInActiveFilter = false;
 
-	for (const line of buffer.map(String)) {
+	for (const line of buffer) {
 		if (line === 'filterActiveInlineStyles') {
 			isInActiveFilter = true;
 		}
@@ -73,7 +74,7 @@
 			const delta = authorData.declarations[0] - activeData.declarations[len];
 			const total = authorData.declarations[0];
 			const quotientInPc = 100 * (1 - delta / total);
-			assert(quotientInPc >= 80);
+			expect(quotientInPc >= 80).toBeTrue();
 		});
 
 		it('saves 80% of styling data bytecount', function() {
@@ -81,50 +82,51 @@
 			const difference = authorData.bytes[0] - activeData.bytes[len];
 			const total = authorData.bytes[0];
 			const quotientInPc = 100 * (1 - difference / total);
-			assert(quotientInPc >= 80);
+			expect(quotientInPc >= 80).toBeTrue();
 		});
 
 		it('longest property excluding vars has 5 hyphens', function() {
 			const declarations = inlineStylesFn();
 			for (const declaration of declarations) {
-				const maxHyphenCount = Object.keys(declaration)
-					.filter(prop => typeof prop === 'string')
-					.filter(prop => !prop.startsWith('-'))
-					.map(prop => prop.split('-').length)
-					.reduce((a, b) => Math.max(a, b));
-				expect(maxHyphenCount <= 5).to.be.true();
+				const maxHyphenCount = Math.max(
+					...Object.keys(declaration)
+						.filter(prop => typeof prop === 'string')
+						.filter(prop => !prop.startsWith('-'))
+						.map(prop => prop.split('-').length)
+				);
+				expect(maxHyphenCount <= 5).toBeTrue();
 			}
 		});
 
 		it('has a runtime of below 4ms/element in the author filter', function() {
 			const count = generalData.elements;
 			const runtime = authorData.runtime;
-			expect(runtime / count).to.be.at.most(4);
+			expect(runtime / count).toBeLessThanOrEqual(4);
 		});
 
 		it('has a runtime of below 8ms/element in the active filter', function() {
 			const count = generalData.elements;
 			const runtime = activeData.runtime;
-			expect(runtime / count).to.be.at.most(8);
+			expect(runtime / count).toBeLessThanOrEqual(8);
 		});
 
 		it('has a runtime of below 3x of the author filter in the active filter', function() {
 			const count = generalData.elements;
 			const authorTimeCost = authorData.runtime / count;
-			const activeTimeCost = authorData.runtime / count;
-			expect(activeTimeCost / authorTimeCost).to.be.at.most(3);
+			const activeTimeCost = activeData.runtime / count;
+			expect(activeTimeCost / authorTimeCost).toBeLessThanOrEqual(3);
 		});
 
 		it('has 4 or less passes in the active filter', function() {
 			const passes = activeData.deltas.length;
-			expect(passes).to.be.at.most(4);
+			expect(passes).toBeLessThanOrEqual(4);
 		});
 
 		it('has each delta less than the previous delta in the active filter', function() {
 			for (i = 2; i < activeData.deltas.length; i++) {
 				const lastDelta = activeData.deltas[i - 1];
 				const currentDelta = activeData.deltas[i];
-				expect(currentDelta).to.be.at.most(lastDelta);
+				expect(currentDelta).toBeLessThanOrEqual(lastDelta);
 			}
 		});
 
@@ -132,8 +134,8 @@
 			const declarations = inlineStylesFn();
 			for (const declaration of declarations) {
 				const properties = Object.keys(declaration);
-				expect('block-size' in properties).to.equal.false;
-				expect('inline-size' in properties).to.equal.false;
+				expect('block-size' in properties).toBeFalse();
+				expect('inline-size' in properties).toBeFalse();
 			}
 		});
 
@@ -141,10 +143,10 @@
 			const declarations = inlineStylesFn();
 			for (const declaration of declarations) {
 				const properties = Object.keys(declaration);
-				expect('margin-block-start' in properties).to.equal.false;
-				expect('margin-block-end' in properties).to.equal.false;
-				expect('margin-inline-start' in properties).to.equal.false;
-				expect('margin-inline-end' in properties).to.equal.false;
+				expect('margin-block-start' in properties).toBeFalse();
+				expect('margin-block-end' in properties).toBeFalse();
+				expect('margin-inline-start' in properties).toBeFalse();
+				expect('margin-inline-end' in properties).toBeFalse();
 			}
 		});
 
@@ -161,7 +163,7 @@
 						const valueBefore = declarationBefore[prop];
 						const valueAfter = declarationAfter[prop];
 
-						expect(valueAfter).to.equal(valueBefore);
+						expect(valueAfter).toEqual(valueBefore);
 					}
 				}
 			}
@@ -170,13 +172,12 @@
 		it('produces determinate results across runs', function() {
 			const prevBuffer = structuredClone(buffer);
 
-			const info = console.info;
-			console.info = infoSpy;
+			console.debug = debugSpy;
 			buffer = [];
 
 			globalThis.dominlinestylefilter.sync(root, { debug: true });
 
-			console.info = info;
+			console.debug = debug;
 
 			let isInAuthorResult = 0;
 			let isInActiveResult = 0;
@@ -202,9 +203,9 @@
 						const timeAfter = line.match(/\d+/);
 						const timeBefore = prevLine.match(/\d+/);
 						const difference = (timeAfter - timeBefore) / timeBefore;
-						expect(difference).to.be.below(0.2).above(-0.2);
+						expect(difference).toBeLessThan(0.2).tobeGreaterThan(-0.2);
 					} else {
-						expect(line).to.equal(prevLine);
+						expect(line).toEqual(prevLine);
 					}
 				}
 			}
